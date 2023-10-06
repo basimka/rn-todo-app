5c2c655f2e28d6b8a1ba3bf10499645f288b0d6f
Temniy Daiver <orcanlend@narod.ru>
2023-10-06 16:26:04 +0300

    modified:   src/AddTodo.js

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,7 +5,7 @@ export const AddTodo = props => {
     return(
         <View style = {styles.block}>
             <TextInput />
-            <Button title = 'ДОБавить ' />
+            <Button title = 'Добавить ' />
 
         </View>
     )
