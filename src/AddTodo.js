3522f7d69cfd3e6c6c0049259aa3d350831a1b8e
dDaiver <bobkovmv@gmail.com>
2023-10-06 11:53:46 +0300

    Update AddTodo.js

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,7 +5,7 @@ export const AddTodo = props => {
     return(
         <View style = {styles.block}>
             <TextInput />
-            <Button title = 'ДОБАВИТЬ ' />
+            <Button title = 'ДОБавить ' />
 
         </View>
     )
