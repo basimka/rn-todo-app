5db943154858e6999cfeac4d70733b891de61ac1
Temniy Daiver <orcanlend@narod.ru>
2024-01-10 17:20:10 +0300

    modified:   App.js 	modified:   src/AddTodo.js

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
 import { Navbar } from './src/Navbar';
 import { AddTodo } from './src/AddTodo';
 import { Todo } from './src/Todo';
 
 
 export default function App() {
-  const [todos,setTodos] = useState([])
+  const [todos,setTodos] = useState([
+    {id:1, title: 'test'},
+    {id:2, title: 'test'},
+    {id:3, title: 'test'},
+    {id:4, title: 'test'},
+    {id:5, title: 'test'},
+    {id:6, title: 'test'},
+    {id:7, title: 'test'},
+    {id:8, title: 'test'},
+    {id:9, title: 'test'},
+    {id:10, title: 'test'},
+    {id:11, title: 'test'},
+    {id:12, title: 'test'},
+  ])
 
   const addTodo = title => {
     //const newTodo = {
@@ -28,13 +41,15 @@ export default function App() {
       <Navbar title="ToDo App" />
       <View style={styles.container}>
         <AddTodo onSubmit={addTodo} />
-        <View>
-          { todos.map(todo =>{
-            return <Text key={todo.id}>{todo.title}</Text>
-            }
-            ) 
-          }
-        </View>
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({item})=>(
+            <Todo todo={item}/>
+          )}
+
+        />
+        
       </View>
     </View>
   );
