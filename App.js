e8f215c4ba094c9937921408c16f650c794ebc0a
Temniy Daiver <orcanlend@narod.ru>
2024-01-10 13:59:56 +0300

    modified:   App.js 	modified:   src/AddTodo.js

--- App.js
+++ App.js
@@ -1,15 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Navbar } from './src/Navbar';
-import React from 'react';
+import React, { useState } from 'react';
 import { AddTodo } from './src/AddTodo';
 
 
 export default function App() {
+  const [todos,setTodos] = useState([])
+
+  const addTodo = title => {
+    //const newTodo = {
+    //  id: Date.now().toString(),
+    //  title
+    //}
+
+    setTodos(prev => [
+      ...prev,
+      {
+      id: Date.now().toString(),
+      title
+    }])
+  }
+
   return (
     <View style={styles.container}>
       <Navbar title="ToDo App" />
-      <AddTodo />
+      <AddTodo onSubmit={AddTodo}/>
+      <View>
+        { todos.map(todo =>{
+          return <Text key={todo.id}>{todo.title}</Text>
+        }) }
+      </View>
     </View>
   );
 }
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { View,StyleSheet,TextInput,Button } from 'react-native'
 
-export const AddTodo = props => {
+export const AddTodo = ({ onSubmint }) => {
+
+    const pressHandler = () =>{
+        onSubmint('Test todo!')
+    }
     return(
         <View style = {styles.block}>
             <TextInput />
-            <Button title = 'Добавить' />
+            <Button title = 'Добавить' onPress={pressHandler}/>
 
         </View>
     )
