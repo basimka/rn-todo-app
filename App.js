325983d6b3b7a0da8a8fef540e3bea9821c78567
Temniy Daiver <orcanlend@narod.ru>
2024-01-12 07:23:40 +0300

    modified:   App.js 	renamed:    src/AddTodo.js -> src/components/AddTodo.js 	renamed:    src/Navbar.js -> src/components/Navbar.js 	renamed:    src/Todo.js -> src/components/Todo.js 	modified:   src/screens/MainScreen.js

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, ScrollView, FlatList} from 'react-native';
-import { Navbar } from './src/Navbar';
-import { AddTodo } from './src/AddTodo';
-import { Todo } from './src/Todo';
+import { Navbar } from './src/components/Navbar';
+import { MainScreen } from './src/screens/MainScreen';
+
 
 
 export default function App() {
@@ -31,13 +31,8 @@ export default function App() {
     <View>
       <Navbar title="ToDo App" />
       <View style={styles.container}>
-        <AddTodo onSubmit={addTodo} />
-        <FlatList
-          keyExtractor={item => item.id.toString()}
-          data={todos}
-          renderItem={({item})=>(<Todo todo={item} onRemove={removeTodo}/> )}
-
-        />
+        <MainScreen todos={todos} addTodo={addTodo} removeTodo={removeTodo}/>
+        
         
       </View>
     </View>
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,9 +1,17 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { AddTodo } from "../components/AddTodo";
+import { Todo } from "../components/Todo";
 
-export const MainScreen = props => {
+export const MainScreen = ({ addTodo, todos, removeTodo}) => {
     return <View>
-        <Text>MainScreen</Text>
+        <AddTodo onSubmit={addTodo} />
+        <FlatList
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({item})=>(<Todo todo={item} onRemove={removeTodo}/> )}
+
+        />
     </View>
 }
 
